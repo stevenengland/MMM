388f2e77a118d061623afbd147680d312ed14202
StEn <StEn@steven-england.info>
2019-08-09 09:28:22 +0200

    test(Integration tests): MobileBuildTools
    Locally set up MBT of DanSiegel for secrets in integration tests

---
+++ src/StEn.MMM.Test/Mql.Telegram.IntegrationTests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mql.Telegram.IntegrationTests.Helpers;
+
+namespace Mql.Telegram.IntegrationTests
+{
+	public class Class1
+	{
+		public void SendTextMessage()
+		{
+			var apikey = Secrets.BOT_API_KEY;
+		}
+	}
+}
