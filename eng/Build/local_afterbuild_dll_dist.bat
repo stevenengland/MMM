ff4c5728ab56bf9dae7c2be09a80949a3aa0b155
StEn <StEn@steven-england.info>
2019-09-02 13:08:51 +0200

    docs(MetaTrader files): Add MT folder structure
    Add MT folder structure to doc so that examples can be easiely integrated in dev
    workflow

--- eng/Build/local_afterbuild_dll_dist.bat
+++ eng/Build/local_afterbuild_dll_dist.bat
@@ -1,12 +1,7 @@
 @echo off
 REM This is only for local tests. 
 REM This batch distributes the dll files to the MataTrader folders where they can be tested.
-REM To use it on your machine, you have to create a file named .local.config.txt that declares the variables %MT4LibFolder% / %MT5LibFolder%
-REM Example: 
-REM		MT4LibFolder="C:\Users\user\AppData\Roaming\MetaTrader\MQL5\Libraries\"
-REM		MT5LibFolder="C:\Users\user\AppData\Roaming\MetaTrader 5\MQL5\Libraries\"
+REM To use it on your machine, you have to execute the symlink batch file first
 
-for /f "delims=" %%x in (.local.config.txt) do (set "%%x")
-
-xcopy /Y "%~dp0..\..\src\StEn.MMM\Mql.Telegram\bin\x64\Release\netstandard2.0\Mql_Telegram.dll" %MT4LibFolder%
-xcopy /Y "%~dp0..\..\src\StEn.MMM\Mql.Telegram\bin\x64\Release\netstandard2.0\Mql_Telegram.dll" %MT5LibFolder%
+xcopy /Y "%~dp0..\..\src\StEn.MMM\Mql.Telegram\bin\x64\Release\netstandard2.0\Mql_Telegram.dll" "%~dp0..\..\doc\module_usage\implementation\metatrader5\MQL5\Libraries\"
+xcopy /Y "%~dp0..\..\src\StEn.MMM\Mql.Telegram\bin\x64\Release\netstandard2.0\Mql_Telegram.dll" "%~dp0..\..\doc\module_usage\implementation\metatrader4\MQL5\Libraries\"
---
+++ eng/Build/local_symlink_dirs.bat
@@ -0,0 +1,12 @@
+@echo off
+REM This is only for local tests. 
+REM This batch creates symbolic links that point to the MT4/5 MQL folder. It's purpose is that you can distribute files build with VS to the folder and they will also be versioned.
+REM To use it on your machine, you have to create a file named .local.config.txt that declares the variables %MT4MQLFolder% / %MT5MQLFolder%
+REM Example: 
+REM		MT4MQLFolder="C:\Users\user\AppData\Roaming\MetaTrader\MQL5\"
+REM		MT5MQLFolder="C:\Users\user\AppData\Roaming\MetaTrader 5\MQL5\"
+
+for /f "delims=" %%x in (.local.config.txt) do (set "%%x")
+
+mklink /J "D:\coding\MMM\doc\module_usage\implementation\metatrader5\MQL5" %MT5MQLFolder%
+mklink /J "D:\coding\MMM\doc\module_usage\implementation\metatrader4\MQL5" %MT4MQLFolder%
